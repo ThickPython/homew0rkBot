2982637034ef2d47769770036bf443af96ca2b32
Colin <colindeng33@gmail.com>
2020-04-09 13:28:46 -0700

    Tweaked the cleaner to work more

--- cleaner.py
+++ cleaner.py
@@ -14,23 +14,21 @@ def deleteOld():
             updatedHomework = teacher["homework"].copy()
             for homework in updatedHomework:
                 homeworkDueDate = homework["duedate"].split("/")
-                if int(todayIs.day) > int(homeworkDueDate[1]) and int(todayIs.month) > int(homeworkDueDate[0]):
+                if int(todayIs.day) > int(homeworkDueDate[1]) and int(todayIs.month) >= int(homeworkDueDate[0]):
                     print(f'removing {homework}')
                     updatedHomework.remove(homework)
-                elif int(todayIs.day) < int(homeworkDueDate[1]) and int(todayIs.month) > int(homeworkDueDate[0]):
+                elif int(todayIs.day) <= int(homeworkDueDate[1]) and int(todayIs.month) > int(homeworkDueDate[0]):
                     print(f'removing {homework}')
                     updatedHomework.remove(homework)
                 elif int(todayIs.day) > int(homeworkDueDate[1]) and int(todayIs.month) > int(homeworkDueDate[0]):
                     print(f'removing {homework}')
                     updatedHomework.remove(homework)
             teacher["homework"] = updatedHomework
-        teacherList.seek(0)
-        json.dump(teacherListDict, teacherList, indent=4)
-        teacherList.truncate()
     uploadFile('teachers.json')
 
 print("cleaner is up and running!")
 
 while True:
-  deleteOld()
-  time.sleep(86400)
+    print("CLEANING TIMEEEEEEEEEEEEEEEEEEEEEE")
+    deleteOld()
+    time.sleep(86400)
