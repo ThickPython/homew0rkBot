2a5722621a3affe9eb2dfcb5a41011b3e9c2e2f9
RezPls <colindeng33@gmail.com>
2020-05-21 21:21:16 -0700

    bugs

--- cleaner.py
+++ cleaner.py
@@ -1,33 +1,28 @@
 import time
 import json
 from datetime import datetime
-from thingsthatcouldbeuseful import get_file, upload_file
+from devutil import *
 
 starttime=time.time()
 
 def deleteOld():
-    get_file('teachers.json')
-    with open('teachers.json', 'r+') as teacherList:
-        teacherListDict = json.load(teacherList)
-        todayIs = datetime.today()
-        for teacher in teacherListDict:
-            updatedHomework = teacher["homework"].copy()
-            for homework in updatedHomework:
-                homeworkDueDate = homework["duedate"].split("/")
-                if int(todayIs.day) > int(homeworkDueDate[1]) and int(todayIs.month) >= int(homeworkDueDate[0]):
-                    print(f'removing {homework}')
-                    updatedHomework.remove(homework)
-                elif int(todayIs.day) <= int(homeworkDueDate[1]) and int(todayIs.month) > int(homeworkDueDate[0]):
-                    print(f'removing {homework}')
-                    updatedHomework.remove(homework)
-                elif int(todayIs.day) > int(homeworkDueDate[1]) and int(todayIs.month) > int(homeworkDueDate[0]):
-                    print(f'removing {homework}')
-                    updatedHomework.remove(homework)
-            teacher["homework"] = updatedHomework
-            teacherList.seek(0)
-            json.dump(teacherListDict, teacherList, indent = 4)
-            teacherList.truncate()
-    upload_file('teachers.json')
+    teacher_list = get_file('teachers.json')
+    todayIs = datetime.today()
+    for teacher in teacher_list:
+        updated_hw = teacher_list[teacher].copy()
+        for homework in updated_hw:
+            homeworkDueDate = homework["duedate"].split("/")
+            if int(todayIs.day) > int(homeworkDueDate[1]) and int(todayIs.month) >= int(homeworkDueDate[0]):
+                print(f'removing {homework}')
+                updated_hw.remove(homework)
+            elif int(todayIs.day) <= int(homeworkDueDate[1]) and int(todayIs.month) > int(homeworkDueDate[0]):
+                print(f'removing {homework}')
+                updated_hw.remove(homework)
+            elif int(todayIs.day) > int(homeworkDueDate[1]) and int(todayIs.month) > int(homeworkDueDate[0]):
+                print(f'removing {homework}')
+                updated_hw.remove(homework)
+        teacher_list[teacher] = updated_hw
+    upload_file(teacher_list, 'teachers.json')
 
 print("cleaner is up and running!")
 
